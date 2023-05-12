324451fe37d2ff472bea3ef4d5aa29ff36ab9a35
Jussi Joki <judejoki@gmail.com>
2023-05-12 21:27:36 +0200

    authenticate

---
+++ AuthenticationService/AuthenticationService/Services/AuthenticateService.cs
@@ -0,0 +1,22 @@
+﻿namespace AuthenticationService.Services;
+
+using Grpc.Core;
+
+public class AuthenticateService : Authenticate.AuthenticateBase
+{
+    private readonly ILogger<AuthenticateService> _logger;
+    public AuthenticateService(ILogger<AuthenticateService> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<AuthenticateReply> Login(AuthenticateRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new AuthenticateReply
+        {
+            Success = true,
+            Id = 1,
+            Message = $"{request.Username} logged in"
+        });
+    }
+}
